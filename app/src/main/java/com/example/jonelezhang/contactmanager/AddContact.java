659dd217980eed1df1b855342fa31ffaeecc2cfc
Jolin Zhang <zhangru0026@gmail.com>
2016-10-29 23:51:07 -0500

    layout change

--- app/src/main/java/com/example/jonelezhang/contactmanager/AddContact.java
+++ app/src/main/java/com/example/jonelezhang/contactmanager/AddContact.java
@@ -4,7 +4,10 @@ package com.example.jonelezhang.contactmanager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatButton;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,14 +17,15 @@ import android.view.ViewGroup;
  */
 
 public class AddContact extends Fragment {
-    private android.support.v7.widget.AppCompatButton save;
+    private AppCompatButton save;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add_contact,container, false);
         setHasOptionsMenu(false);
 
-        save = (android.support.v7.widget.AppCompatButton)getActivity().findViewById(R.id.save);
+        save = (AppCompatButton) v.findViewById(R.id.save);
+        setHasOptionsMenu(true);
 
         return v;
     }
@@ -32,9 +36,26 @@ public class AddContact extends Fragment {
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                
-
+                getActivity().getSupportFragmentManager().popBackStack();
             }
         });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.delete_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.delete:
+                getActivity().getSupportFragmentManager().popBackStack();
+                return true;
+            default:
+                return false;
+
+        }
     }
 }
