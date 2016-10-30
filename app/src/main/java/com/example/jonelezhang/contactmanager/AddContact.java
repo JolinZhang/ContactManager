72c1efd0817044b9b25de7ce30823c24c6358613
Jolin Zhang <zhangru0026@gmail.com>
2016-10-30 00:04:14 -0500

    add delete button

--- app/src/main/java/com/example/jonelezhang/contactmanager/AddContact.java
+++ app/src/main/java/com/example/jonelezhang/contactmanager/AddContact.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.AppCompatButton;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 /**
  * Created by Jonelezhang on 10/28/16.
@@ -18,15 +21,32 @@ import android.view.ViewGroup;
 
 public class AddContact extends Fragment {
     private AppCompatButton save;
+    private EditText firstName;
 
     @Override
     public View onCreateView(LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_add_contact,container, false);
-        setHasOptionsMenu(false);
-
-        save = (AppCompatButton) v.findViewById(R.id.save);
         setHasOptionsMenu(true);
 
+        save = (AppCompatButton) v.findViewById(R.id.save);
+        firstName = (EditText) v.findViewById(R.id.firstName);
+        firstName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                save.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         return v;
     }
 
